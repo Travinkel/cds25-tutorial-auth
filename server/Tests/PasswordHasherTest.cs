b79359afe5a53c1a5a5ce5b6340c746e850a9ce8
Stefan Ankersø <57225948+Travinkel@users.noreply.github.com>
2025-10-31 16:51:59 +0000

    test(auth): add unit tests for password hasher and sanity check

---
+++ server/Tests/PasswordHasherTest.cs
@@ -0,0 +1,35 @@
+using Api;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using TUnit;
+using TUnit.Assertions;
+
+# pragma warning disable CS8625, CS8618
+
+namespace Tests.Security;
+
+public class PasswordHasherTest
+{
+    IPasswordHasher<User> sut;
+
+    [Before(Test)]
+    public void Setup()
+    {
+        var builder = WebApplication.CreateBuilder();
+        Program.ConfigureServices(builder);
+        var app = builder.Build();
+        sut = app.Services.GetRequiredService<IPasswordHasher<User>>();
+        Console.WriteLine($"Using password hasher: {sut.GetType().Name}");
+    }
+
+    [Test]
+    public async Task HashAndVerifyPassword()
+    {
+        var password = "S3cret!1";
+        var hash = sut.HashPassword(null, password);
+        var result = sut.VerifyHashedPassword(null, hash, password);
+        await Assert.That(result).IsEqualTo(PasswordVerificationResult.Success);
+    }
+}
---
+++ server/Tests/SanityTest.cs
@@ -0,0 +1,13 @@
+/*using TUnit;
+using TUnit.Assertions;
+
+namespace Tests;
+
+public class SanityTest
+{
+    [Test]
+    public async Task Should_Run()
+    {
+        await Assert.That(true).IsTrue();
+    }
+}*/
